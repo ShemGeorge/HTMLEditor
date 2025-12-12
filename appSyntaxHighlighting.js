6bfb37bb309e8d4bd5873f0db2aff3a24e4cdb7b
ShemGeorge <shems.harmony@gmail.com>
2025-12-12 14:32:06 +0530

    Update appSyntaxHighlighting.js

--- appSyntaxHighlighting.js
+++ appSyntaxHighlighting.js
@@ -116,8 +116,149 @@ str = str.substr(e + (end.length));
 this.rest = d + str;
 this.arr = a;
 }
+function replaceGreaterThanInQuotes(txt) {
+function isEligibleQuote(str, pos) {
+if (pos === 0) {
+return false;
+}
+let j = pos - 1;
+while (j >= 0 && str[j] === ' ') {
+j--;
+}
+if (j < 0 || str[j] !== '=') {
+return false;
+}
+let open = str.lastIndexOf("&lt;", j);
+if (open === -1) {
+open = 0;
+}
+for (let i = open; i < j; i++) {
+if (str[i] === ' ') {
+return true;
+}
+}
+return false;
+}
+function findTagEnd(str, startIndex) {
+let inSingle = false, inDouble = false;
+let i = startIndex + 4;
+while (i < str.length) {
+let ch = str[i];
+if (!inSingle && ch === '"' && isEligibleQuote(str, i)) {
+inDouble = true;
+i++;
+continue;
+}
+if (!inDouble && ch === "'" && isEligibleQuote(str, i)) {
+inSingle = true;
+i++;
+continue;
+}
+if (inDouble && ch === '"') {
+inDouble = false;
+i++;
+continue;
+}
+if (inSingle && ch === "'") {
+inSingle = false;
+i++;
+continue;
+}
+if (!inSingle && !inDouble && str.startsWith("&gt;", i)) {
+return i;
+}
+i++;
+}
+return -1;
+}
+function extractFullBlocks(source, tagName, placeholder, storeArray) {
+let out = "", i = 0;
+const openToken = `&lt;${tagName}`, closeToken = `&lt;/${tagName}&gt;`;
+while (i < source.length) {
+let openIndex = source.toLowerCase().indexOf(openToken, i);
+if (openIndex === -1) {
+out += source.substring(i);
+break;
+}
+out += source.substring(i, openIndex);
+let endOpen = findTagEnd(source, openIndex);
+if (endOpen === -1) {
+out += source.substring(openIndex);
+break;
+}
+let openTag = source.substring(openIndex, endOpen + 4);
+let closeIndex = source.toLowerCase().indexOf(closeToken, endOpen + 4);
+if (closeIndex === -1) {
+out += source.substring(openIndex);
+break;
+}
+let inner = source.substring(endOpen + 4, closeIndex);
+storeArray.push(inner);
+let closeTag = source.substring(closeIndex, closeIndex + closeToken.length);
+out += openTag + placeholder + closeTag;
+i = closeIndex + closeToken.length;
+}
+return out;
+}
+let scripts = [], styles = [];
+txt = extractFullBlocks(txt, "script", "\uF006", scripts);
+txt = extractFullBlocks(txt, "style", "\uF007", styles);
+let result = "", pos = 0;
+while (pos < txt.length) {
+let tagStart = txt.indexOf("&lt;", pos);
+if (tagStart === -1) {
+result += txt.substring(pos);
+break;
+}
+result += txt.substring(pos, tagStart);
+let tagEnd = findTagEnd(txt, tagStart);
+if (tagEnd === -1) {
+result += txt.substring(tagStart).split("&gt;").join("\u0000");
+break;
+}
+let tagText = txt.substring(tagStart, tagEnd + 4);
+let processed = "";
+let inSingle = false, inDouble = false;
+for (let i = 0; i < tagText.length; i++) {
+let ch = tagText[i];
+if (!inSingle && ch === '"' && isEligibleQuote(tagText, i)) {
+inDouble = true;
+processed += ch;
+continue;
+}
+if (!inDouble && ch === "'" && isEligibleQuote(tagText, i)) {
+inSingle = true;
+processed += ch;
+continue;
+}
+if (inDouble && ch === '"') {
+inDouble = false;
+processed += ch;
+continue;
+}
+if (inSingle && ch === "'") {
+inSingle = false;
+processed += ch;
+continue;
+}
+if ((inSingle || inDouble) && tagText.startsWith("&gt;", i) && i < tagText.length - 4) {
+processed += "\u0000"; i += 3; continue;
+}
+processed += ch;
+}
+result += processed;
+pos = tagEnd + 4;
+}
+for (var s of scripts) {
+result = result.replace("\uF006", s);
+}
+for (var s of styles) {
+result = result.replace("\uF007", s);
+}
+return result;
+}
 function htmlMode(txt) {
-var rest = txt, done = "", php, comment, angular, startpos, endpos, note, i;
+var rest = txt, done = "", comment, startpos, endpos, note, i;
 rest = disableHTMLCommentsInScriptAndStyle(rest);
 comment = new extract(rest, "&lt;!--", "--&gt;", commentMode, "\uF001");
 rest = comment.rest;
@@ -129,26 +270,33 @@ done += "<span class='html-doctype-" + theme.value + "'>" + rest.substring(0, en
 rest = rest.substr(endpos + 4);
 continue;
 }
+rest = replaceGreaterThanInQuotes(rest);
 note = "";
 startpos = rest.indexOf("&lt;");
-if (rest.substr(startpos, 9).toUpperCase() === "&LT;STYLE" && (rest.substr(startpos + 9, 4).toUpperCase().startsWith("&GT;") || rest.charAt(startpos + 9) === " ")) {note = "css";}
-if (rest.substr(startpos, 10).toUpperCase() === "&LT;SCRIPT" && ( rest.substr(startpos + 10, 4).toUpperCase().startsWith("&GT;") || rest.charAt(startpos + 10) === " " )) {note = "javascript";}
+if (rest.substr(startpos, 9).toUpperCase() === "&LT;STYLE" && (rest.substr(startpos + 9, 4).toUpperCase().startsWith("&GT;") || rest.charAt(startpos + 9) === " ")) {
+note = "css";
+}
+if (rest.substr(startpos, 10).toUpperCase() === "&LT;SCRIPT" && (rest.substr(startpos + 10, 4).toUpperCase().startsWith("&GT;") || rest.charAt(startpos + 10) === " ")) {
+note = "javascript";
+}
 endpos = rest.indexOf("&gt;", startpos);
-if (endpos == -1) {endpos = rest.length;}
+if (endpos == -1) {
+endpos = rest.length;
+}
 done += rest.substring(0, startpos);
 done += tagMode(rest.substring(startpos, endpos + 4));
 rest = rest.substr(endpos + 4);
-if (note == "css") {
+if (note === "css") {
 endpos = rest.toUpperCase().indexOf("&LT;/STYLE&GT;");
-if (endpos == -1) {
+if (endpos === -1) {
 endpos = rest.length;
 }
 done += cssMode(rest.substring(0, endpos));
 rest = rest.substr(endpos);
 }
-if (note == "javascript") {
+if (note === "javascript") {
 endpos = rest.toUpperCase().indexOf("&LT;/SCRIPT&GT;");
-if (endpos == -1) {
+if (endpos === -1) {
 endpos = rest.length;
 }
 done += jsMode(rest.substring(0, endpos));
@@ -160,6 +308,7 @@ for (i = 0; i < comment.arr.length; i++) {
 rest = rest.replace("\uF001", comment.arr[i]);
 }
 rest = rest.replace(/\u2063/g, "");
+rest = rest.replace(/\u0000/g, "&gt;");
 return rest;
 }
 function tagMode(txt) {
@@ -184,24 +333,31 @@ result = result.substring(0, result.length - 4) + "<span class='html-bracket-" +
 return "<span class='html-tag-" + theme.value + "'>" + result + "</span>";
 }
 function attributeMode(txt) {
-var rest = txt, done = "", startpos, endpos, singlefnuttpos, doublefnuttpos, spacepos;
-while (rest.indexOf("=") > -1) {
-endpos = -1;
-startpos = rest.indexOf("=");
-singlefnuttpos = rest.indexOf("'", startpos);
-doublefnuttpos = rest.indexOf('"', startpos);
-spacepos = rest.indexOf(" ", startpos + 2);
-if (spacepos > -1 && (spacepos < singlefnuttpos || singlefnuttpos == -1) && (spacepos < doublefnuttpos || doublefnuttpos == -1)) {
-endpos = rest.indexOf(" ", startpos);
-} else if (doublefnuttpos > -1 && (doublefnuttpos < singlefnuttpos || singlefnuttpos == -1) && (doublefnuttpos < spacepos || spacepos == -1)) {
-endpos = rest.indexOf('"', rest.indexOf('"', startpos) + 1);
-} else if (singlefnuttpos > -1 && (singlefnuttpos < doublefnuttpos || doublefnuttpos == -1) && (singlefnuttpos < spacepos || spacepos == -1)) {
-endpos = rest.indexOf("'", rest.indexOf("'", startpos) + 1);
+let rest = txt;
+let done = "";
+while (true) {
+let eqPos = rest.indexOf("=");
+if (eqPos === -1) break;
+done += rest.substring(0, eqPos);
+let valStart = eqPos + 1;
+while (rest[valStart] === " ") {
+valStart++;
 }
-if (!endpos || endpos == -1 || endpos < startpos) {endpos = rest.length;}
-done += rest.substring(0, startpos);
-done += attributeValueMode(rest.substring(startpos, endpos + 1));
-rest = rest.substr(endpos + 1);
+let endpos = -1;
+if (rest[valStart] === '"') {
+endpos = rest.indexOf('"', valStart + 1);
+} else if (rest[valStart] === "'") {
+endpos = rest.indexOf("'", valStart + 1);
+} else {
+let space = rest.indexOf(" ", valStart);
+endpos = space === -1 ? rest.length : space - 1;
+}
+if (endpos === -1) {
+endpos = rest.length - 1;
+}
+let attrSegment = rest.substring(eqPos, endpos + 1);
+done += attributeValueMode(attrSegment);
+rest = rest.substring(endpos + 1);
 }
 return "<span class='html-attribute-" + theme.value + "'>" + done + rest + "</span>";
 }
@@ -211,8 +367,41 @@ return "<span class='html-attributeEquals-" + theme.value + "'>=</span><span cla
 function commentMode(txt) {
 return "<span class='comment-" + theme.value + "'>" + txt + "</span>";
 }
+function makeCssSafe(text, colonPlaceholder = "\uF00C", semicolonPlaceholder = "\uF00D", openingBracePlaceholder = "\uF00E", closingBracePlaceholder = "\uF00F") {
+let inSingle = false, inDouble = false, inBacktick = false;
+let result = "";
+text = text.replace(/&lt;/g,"\uF008").replace(/&gt;/g,"\uF009").replace(/&amp;/g, "\uF00A").replace(/&nbsp;/g,"\uF00B");
+for (let i = 0; i < text.length; i++) {
+let ch = text[i];
+if (ch === '"' && !inSingle && !inBacktick) {
+inDouble = !inDouble;
+}
+else if (ch === "'" && !inDouble && !inBacktick) {
+inSingle = !inSingle;
+}
+else if (ch === "`" && !inDouble && !inSingle) {
+inBacktick = !inBacktick;
+}
+if ((inSingle || inDouble || inBacktick) && ch === ";") {
+result += semicolonPlaceholder;
+}
+else if ((inSingle || inDouble || inBacktick) && ch === "}") {
+result += closingBracePlaceholder;
+}
+else if ((inSingle || inDouble || inBacktick) && ch === "{") {
+result += openingBracePlaceholder;
+}
+else if ((inSingle || inDouble || inBacktick) && ch === ":") {
+result += colonPlaceholder;
+}
+else {
+result += ch;
+}
+}
+return result;
+}
 function cssMode(txt) {
-var rest = txt, done = "", s, e, comment, i, midz, c, cc;
+var rest = makeCssSafe(txt), done = "", s, e, comment, i, midz, c, cc;
 comment = new extract(rest, /\/\*/, "*/", commentMode, "\uF002");
 rest = comment.rest;
 while (rest.search("{") > -1) {
@@ -221,16 +410,27 @@ midz = rest.substr(s + 1);
 cc = 1;
 c = 0;
 for (i = 0; i < midz.length; i++) {
-if (midz.substr(i, 1) == "{") {cc++; c++}
-if (midz.substr(i, 1) == "}") {cc--;}
-if (cc == 0) {break;}
+if (midz.substr(i, 1) == "{") {
+cc++;
+c++;
 }
-if (cc != 0) {c = 0;}
+if (midz.substr(i, 1) == "}") {
+cc--;
+}
+if (cc == 0) {
+break;
+}
+}
+if (cc != 0) {
+c = 0;
+}
 e = s;
 for (i = 0; i <= c; i++) {
 e = rest.indexOf("}", e + 1);
 }
-if (e == -1) {e = rest.length;}
+if (e == -1) {
+e = rest.length;
+}
 done += rest.substring(0, s + 1);
 done += cssPropertyMode(rest.substring(s + 1, e));
 rest = rest.substr(e);
@@ -241,102 +441,126 @@ rest = rest.replace(/}/g, "<span class='css-delimiter-" + theme.value + "'>}</sp
 for (i = 0; i < comment.arr.length; i++) {
 rest = rest.replace("\uF002", comment.arr[i]);
 }
+rest = rest.replace(/\uF00C/g, ":").replace(/\uF00D/g, ";").replace(/\uF00E/g, "{").replace(/\uF00F/g, "}").replace(/\uF008/g, "&lt;").replace(/\uF009/g, "&gt;").replace(/\uF00A/g, "&amp;").replace(/\uF00B/g, "&nbsp;");
 return "<span class='css-selector-" + theme.value + "'>" + rest + "</span>";
 }
 function cssPropertyMode(txt) {
-var rest = txt, done = "", s, e, n, loop;
-if (rest.indexOf("{") > -1 ) { return cssMode(rest); }
-while (rest.search(":") > -1) {
-s = rest.search(":");
-loop = true;
-n = s;
-while (loop == true) {
-loop = false;
-e = rest.indexOf(";", n);
-if (rest.substring(e - 5, e + 1) == "&nbsp;") {
-loop = true;
-n = e + 1;
+let rest = txt, done = "";
+let s, e;
+if (rest.indexOf("{") > -1) {
+return cssMode(rest);
 }
-}
-if (e == -1) {e = rest.length;}
+while ((s = rest.indexOf(":")) > -1) {
+e = rest.indexOf(";", s);
+if (e === -1) e = rest.length;
 done += rest.substring(0, s);
 done += cssPropertyValueMode(rest.substring(s, e + 1));
-rest = rest.substr(e + 1);
+rest = rest.substring(e + 1);
 }
 return "<span class='css-property-" + theme.value + "'>" + done + rest + "</span>";
 }
 function cssPropertyValueMode(txt) {
-var rest = txt, done = "", s;
-rest = "<span class='css-delimiter-" + theme.value + "'>:</span>" + rest.substring(1);
-result = done + rest;
-if (result.substr(result.length - 1, 1) == ";" && result.substr(result.length - 6, 6) != "&nbsp;" && result.substr(result.length - 4, 4) != "&lt;" && result.substr(result.length - 4, 4) != "&gt;" && result.substr(result.length - 5, 5) != "&amp;") {
-result = result.substring(0, result.length - 1) + "<span class='css-delimiter-" + theme.value + "'>;</span>";
+if (!txt) return "";
+let value = "<span class='css-delimiter-" + theme.value + "'>:</span>"
++ txt.substring(1);
+let last = value.indexOf(";");
+if (last !== -1) {
+value = value.substring(0, last) + "<span class='css-delimiter-" + theme.value + "'>;</span>" + value.substring(last + 1);
 }
-return "<span class='css-propertyValue-" + theme.value + "'>" + result + "</span>";
+return "<span class='css-propertyValue-" + theme.value + "'>" + value + "</span>";
 }
 function jsMode(txt) {
-var rest = txt, done = "", multilineCommentRanges = [], singlelineCommentRanges = [], idx = 0, escNormal = [], escNewline = [], i, cc, tt = "", sfnuttpos, dfnuttpos, tfnuttpos, compos, comlinepos, regexpos, keywordpos, numpos, mypos, dotpos, y;
+var rest = txt, done = "", multilineCommentRanges = [], singlelineCommentRanges = [], idx = 0,
+escNormal = [], escNewline = [], escBacktick = [], i, cc, tt = "", sfnuttpos, dfnuttpos, tfnuttpos, compos,
+comlinepos, regexpos, keywordpos, numpos, mypos, dotpos, y;
 while (idx < rest.length) {
-let start = rest.indexOf("/*", idx);
-if (start === -1) {
-break;
-}
-let end = rest.indexOf("*/", start + 2);
-if (end === -1) {
-end = rest.length;
-}
-else {
-end += 2;
-}
+var start = rest.indexOf("/*", idx);
+if (start === -1) break;
+var end = rest.indexOf("*/", start + 2);
+if (end === -1) end = rest.length;
+else end += 2;
 multilineCommentRanges.push({ start, end });
 idx = end;
 }
 idx = 0;
 while (idx < rest.length) {
-let start = rest.indexOf("//", idx);
-if (start === -1) {
-break;
-}
-let end = rest.indexOf("\n", start + 2);
-if (end === -1) {
-end = rest.length;
-}
+var start = rest.indexOf("//", idx);
+if (start === -1) break;
+var end = rest.indexOf("\n", start + 2);
+if (end === -1) end = rest.length;
 singlelineCommentRanges.push({ start, end });
 idx = end;
 }
-for (let i = 0; i < rest.length; i++) {
-let cc = rest[i];
-let inMultilineComment = false;
-let inSinglelineComment = false;
-for (let c = 0; c < multilineCommentRanges.length; c++) {
-if (i >= multilineCommentRanges[c].start && i < multilineCommentRanges[c].end) {
-inMultilineComment = true;
-break;
+for (i = 0; i < rest.length; i++) {
+cc = rest[i];
+var inMultilineComment = false, inSinglelineComment = false;
+for (var c = 0; c < multilineCommentRanges.length; c++) {
+if (i >= multilineCommentRanges[c].start && i < multilineCommentRanges[c].end) { inMultilineComment = true; break; }
 }
-}
-for (let c = 0; c < singlelineCommentRanges.length; c++) {
-if (i >= singlelineCommentRanges[c].start && i < singlelineCommentRanges[c].end) {
-inSinglelineComment = true;
-break;
-}
+for (var c = 0; c < singlelineCommentRanges.length; c++) {
+if (i >= singlelineCommentRanges[c].start && i < singlelineCommentRanges[c].end) { inSinglelineComment = true; break; }
 }
 if (cc === "\\" && !inMultilineComment && !inSinglelineComment) {
-if (i + 1 < rest.length && rest[i + 1] === "\n") {
-escNewline.push("\\\n");
-cc = "\uF004";
-i += 1;
-}
-else if (i + 1 < rest.length) {
-escNormal.push(rest.substr(i, 2));
-cc = "\uF003";
-i += 1;
-}
+if (i + 1 < rest.length && rest[i + 1] === "\n") { escNewline.push("\\\n"); cc = "\uF004"; i += 1; }
+else if (i + 1 < rest.length) { escNormal.push(rest.substr(i, 2)); cc = "\uF003"; i += 1; }
 }
 tt += cc;
 }
 rest = tt;
+function inlineTemplateParsing(txt) {
+var out = "", i = 0;
+while (i < txt.length) {
+var c = txt[i];
+if (c === "`") {
+out += c;
+i++;
+while (i < txt.length) {
+c = txt[i];
+if (c === "\\") {
+if (i + 1 < txt.length && txt[i + 1] === "\n") {
+escNewline.push("\\\n");
+out += "\uF004";
+i += 2;
+continue;
+}
+else if (i + 1 < txt.length) {
+escNormal.push(txt.substr(i, 2));
+out += "\uF003";
+i += 2;
+continue;
+}
+}
+if (c === "$" && i + 1 < txt.length && txt[i + 1] === "{") {
+out += "${"; i += 2;
+var braceCount = 1;
+while (i < txt.length && braceCount > 0) {
+var cc = txt[i];
+if (cc === "{") braceCount++;
+else if (cc === "}") braceCount--;
+if (cc === "`") { escBacktick.push("`"); cc = "\uF005"; }
+if (cc === "\\" && i + 1 < txt.length) {
+if (txt[i + 1] === "\n") { escNewline.push("\\\n"); cc = "\uF004"; i++; }
+else { escNormal.push(txt.substr(i, 2)); cc = "\uF003"; i++; }
+}
+out += cc;
+i++;
+}
+continue;
+}
+out += c;
+i++;
+if (c === "`") break;
+}
+continue;
+}
+out += c;
+i++;
+}
+return out;
+}
+rest = inlineTemplateParsing(rest);
 y = 1;
-while (y == 1) {
+while (y === 1) {
 regexpos = getRegexPos(rest, jsRegexMode);
 sfnuttpos = getPos(rest, "'", "'", jsStringMode);
 dfnuttpos = getPos(rest, '"', '"', jsStringMode);
@@ -346,22 +570,22 @@ comlinepos = getPos(rest, /\/\//, "\n", commentMode);
 numpos = getNumPos(rest, jsNumberMode);
 keywordpos = getKeywordPos("js", rest, jsKeywordMode);
 dotpos = getDotPos(rest, jsPropertyMode);
-if (Math.max(numpos[0], sfnuttpos[0], dfnuttpos[0], tfnuttpos[0], compos[0], comlinepos[0], regexpos[0], keywordpos[0], dotpos[0]) == -1) {break;}
+if (Math.max(numpos[0], sfnuttpos[0], dfnuttpos[0], tfnuttpos[0], compos[0], comlinepos[0], regexpos[0], keywordpos[0], dotpos[0]) === -1) {
+break;
+}
 mypos = getMinPos(numpos, sfnuttpos, dfnuttpos, tfnuttpos, compos, comlinepos, regexpos, keywordpos, dotpos);
-if (mypos[0] == -1) {break;}
-if (mypos[0] > -1) {
+if (mypos[0] === -1) {
+break;
+}
 done += rest.substring(0, mypos[0]);
 done += mypos[2](rest.substring(mypos[0], mypos[1]));
 rest = rest.substr(mypos[1]);
 }
-}
 rest = done + rest;
-for (i = 0; i < escNormal.length; i++) {
-rest = rest.replace("\uF003", escNormal[i]);
-}
-for (i = 0; i < escNewline.length; i++) {
-rest = rest.replace("\uF004", escNewline[i]);
-}
+for (i = 0; i < escNormal.length; i++) rest = rest.replace("\uF003", escNormal[i]);
+for (i = 0; i < escNewline.length; i++) rest = rest.replace("\uF004", escNewline[i]);
+for (i = 0; i < escBacktick.length; i++) rest = rest.replace("\uF005", escBacktick[i]);
+
 return rest;
 }
 function jsStringMode(txt) {
